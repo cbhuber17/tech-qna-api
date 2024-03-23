4777ca05322d4fcbb8c79bdb13480b964dec60ef
Colin Huber <cbhuber@gmail.com>
2024-03-23 13:50:26 -0600

    feat: Adding Q&A DAOs to access DB

---
+++ src/persistance/answers_dao.rs
@@ -0,0 +1,105 @@
+use async_trait::async_trait;
+use axum::routing::post;
+use sqlx::PgPool;
+
+use crate::models::{postgres_error_codes, Answer, AnswerDetail, DBError};
+
+#[async_trait]
+pub trait AnswersDao {
+    async fn create_answer(&self, answer: Answer) -> Result<AnswerDetail, DBError>;
+    async fn delete_answer(&self, answer_uuid: String) -> Result<(), DBError>;
+    async fn get_answers(&self, question_uuid: String) -> Result<Vec<AnswerDetail>, DBError>;
+}
+
+pub struct AnswersDaoImpl {
+    db: PgPool,
+}
+
+impl AnswersDaoImpl {
+    pub fn new(db: PgPool) -> Self {
+        AnswersDaoImpl {db} 
+    }
+}
+
+#[async_trait]
+impl AnswersDao for AnswersDaoImpl {
+    async fn create_answer(&self, answer: Answer) -> Result<AnswerDetail, DBError> {
+
+        // Attempt to get question UUID (for the answer), make sure it is valid
+        let uuid = sqlx::types::Uuid::parse_str(&answer.question_uuid).map_err(|_| {
+            DBError::InvalidUUID(format!("Could not parse answer UUID: {}", answer.question_uuid))
+        })?;
+
+        // If executing the query results in an error, check to see if
+        // the error code matches `postgres_error_codes::FOREIGN_KEY_VIOLATION`.
+        // If so early return the `DBError::InvalidUUID` error. Otherwise early return
+        // the `DBError::Other` error.
+        let record = sqlx::query!(
+            r#"
+                INSERT INTO answers ( question_uuid, content )
+                VALUES ( $1, $2 )
+                RETURNING *
+            "#,
+            uuid,
+            answer.content
+        ).fetch_one(&self.db)
+         .await
+         .map_err(|e: sqlx::Error| match e {
+            sqlx::Error::Database(e) => {
+                if let Some(code) = e.code() {
+                    if code.eq(postgres_error_codes::FOREIGN_KEY_VIOLATION) {
+                        return DBError::InvalidUUID(format!("Invalid question UUID: {}", answer.question_uuid));
+                    }
+                }
+                DBError::Other(Box::new(e))
+            }
+            e => DBError::Other(Box::new(e)),
+         })?;
+
+        // Return created record
+        Ok(AnswerDetail {
+            answer_uuid: record.answer_uuid.to_string(),
+            question_uuid: record.question_uuid.to_string(),
+            content: record.content,
+            created_at: record.created_at.to_string(),
+        })
+    }
+
+    async fn delete_answer(&self, answer_uuid: String) -> Result<(), DBError> {
+
+        // Attempt to get the answer UUID, make sure it is valid
+        let uuid = sqlx::types::Uuid::parse_str(&answer_uuid).map_err(|_| {
+            DBError::InvalidUUID(format!("Could not parse answer UUID: {}", answer_uuid))
+        })?;
+
+        // Delete from DB
+        sqlx::query!("DELETE FROM answers WHERE answer_uuid = $1", uuid).execute(&self.db)
+                                                                        .await
+                                                                        .map_err(|e| DBError::Other(Box::new(e)))?;
+
+        Ok(())
+    }
+
+    async fn get_answers(&self, question_uuid: String) -> Result<Vec<AnswerDetail>, DBError> {
+
+        // Attempt to get question UUID (for the answer), make sure it is valid
+        let uuid = sqlx::types::Uuid::parse_str(&question_uuid).map_err(|_| {
+            DBError::InvalidUUID(format!("Could not parse question with UUID: {}", question_uuid))
+        })?;
+
+        // Get all answers from DB
+        let records = sqlx::query!("SELECT * FROM answers WHERE question_uuid = $1", uuid).fetch_all(&self.db)
+                                                                                                       .await
+                                                                                                       .map_err(|e| DBError::Other(Box::new(e)))?;
+
+        // Put the records in an array of AnswerDetail
+        let answers = records.iter().map(|r| AnswerDetail {
+            answer_uuid: r.answer_uuid.to_string(),
+            question_uuid: r.question_uuid.to_string(),
+            content: r.content.clone(),
+            created_at: r.created_at.to_string(),
+        }).collect();
+
+        Ok(answers)
+    }
+}
