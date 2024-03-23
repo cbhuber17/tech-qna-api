3da1cc66f1fc01abb40d188f84536d6c201c7bbf
Colin Huber <cbhuber@gmail.com>
2024-03-23 13:49:52 -0600

    feat: Adding DBError enum and fix title

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,9 @@
+use thiserror::Error;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Question {
-    pub titie: String,
+    pub title: String,
     pub description: String,
 }
 
@@ -38,4 +39,17 @@ pub struct AnswerDetail {
 #[derive(Serialize, Deserialize)]
 pub struct AnswerId {
     pub answer_uuid: String,
+}
+
+#[derive(Error, Debug)]
+pub enum DBError {
+    #[error("Invalid UUID provided: {0}")]
+    InvalidUUID(String),
+    #[error("Database error occurred")]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync>),
+}
+
+// source: https://www.postgresql.org/docs/current/errcodes-appendix.html
+pub mod postgres_error_codes {
+    pub const FOREIGN_KEY_VIOLATION: &str = "23503";
 }
