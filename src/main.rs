3f87e8f37927e17b451aa77ae03e50d307fe56ac
Colin Huber <cbhuber@gmail.com>
2024-03-23 13:50:50 -0600

    feat: Bring in DB persistence into scope

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use dotenvy::dotenv;
 
 mod handlers;
 mod models;
+mod persistance;
 
 use handlers::*;
 use sqlx::postgres::PgPoolOptions;
@@ -19,25 +20,16 @@ use sqlx::postgres::PgPoolOptions;
 #[tokio::main]
 async fn main() {
     const MAX_CONNECTIONS: u32 = 5;
+
     pretty_env_logger::init();
     dotenv().ok();
 
-    // Create a new PgPoolOptions instance with a maximum of 5 connections.
+    // Create a new PgPoolOptions instance
     let pool = PgPoolOptions::new().max_connections(MAX_CONNECTIONS)
                                                    .connect(&std::env::var("DATABASE_URL")
                                                    .expect("DATABASE_URL must be set."))
                                                    .await
                                                    .expect("Failed to create Postgres connection pool!");
-
-
-    // Using slqx, execute a SQL query that selects all questions from the questions table.
-    // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
-    // make sure we can connect to the database.
-    // let recs = todo!();
-    let recs = sqlx::query!("SELECT * FROM questions").fetch_all(&pool).await.unwrap();
-
-    info!("********* Question Records *********");
-    info!("{:?}", recs);
 
     let app = Router::new()
         .route("/question", post(create_question))
