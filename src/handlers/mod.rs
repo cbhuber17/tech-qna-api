4f9e9003a0c1920d901b6cbbd51735dea2782c40
Colin Huber <cbhuber@gmail.com>
2024-03-23 19:05:29 -0600

    feat: DAO implementation

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,105 +1,75 @@
-use crate::models::*;
-use axum::{response::IntoResponse, Json};
+use axum::{
+    extract::State as AxumState, http::StatusCode, response::IntoResponse, Json as JsonAxum,
+};
+
+use crate::{models::*, AppState};
+
+mod handlers_inner;
+
+impl IntoResponse for handlers_inner::HandlerError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            handlers_inner::HandlerError::BadRequest(msg) => {
+                (StatusCode::BAD_REQUEST, msg).into_response()
+            }
+            handlers_inner::HandlerError::InternalError(msg) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, msg).into_response()
+            }
+        }
+    }
+}
 
 // ---- CRUD for Questions ----
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    Json(QuestionDetail {
-        question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-        title: "Lorem ipsum".into(),
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-            tempor incididunt ut labore et dolore magna aliqua. Auctor urna nunc id cursus. Non \
-            odio euismod lacinia at quis. Augue lacus viverra vitae congue eu consequat ac felis. \
-            Justo nec ultrices dui sapien."
-            .into(),
-        created_at: "Jan 1, 1970".into(),
-    })
+pub async fn create_question(
+    // Example of how to add state to a route. Note that we are using ".." to ignore the other fields in AppState.
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+    JsonAxum(question): JsonAxum<Question>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_question(question, questions_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn read_questions() -> impl IntoResponse {
-    Json(vec![
-        QuestionDetail {
-            question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            title: "Lorem ipsum".into(),
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                tempor incididunt ut labore et dolore magna aliqua. Est velit egestas dui id \
-                ornare arcu odio. Suspendisse interdum consectetur libero id faucibus nisl. \
-                Tincidunt vitae semper quis lectus. Proin gravida hendrerit lectus a."
-                .into(),
-            created_at: "Jan 1, 1970".into(),
-        },
-        QuestionDetail {
-            question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            title: "Lorem ipsum".into(),
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                tempor incididunt ut labore et dolore magna aliqua. Ipsum faucibus vitae aliquet \
-                nec ullamcorper sit amet risus nullam. Pulvinar elementum integer enim neque \
-                volutpat ac tincidunt vitae semper. Scelerisque in dictum non consectetur a. Enim \
-                nunc faucibus a pellentesque sit amet porttitor eget."
-                .into(),
-            created_at: "Jan 1, 1970".into(),
-        },
-    ])
+
+pub async fn read_questions(
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_questions(questions_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    ()
+pub async fn delete_question(
+    AxumState(AppState { questions_dao, .. }): AxumState<AppState>,
+    JsonAxum(question_uuid): JsonAxum<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_question(question_uuid, questions_dao.as_ref()).await
 }
 
 // ---- CRUD for Answers ----
 
-// TODO: Create a POST route to /answer which accepts an `Answer` and returns `AnswerDetail` as JSON.
-//       The handler function should be called `create_answer`.
-//
-//       hint: this function should look very similar to the create_question function above
-
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
-    Json(AnswerDetail {
-        answer_uuid: "00000000-0000-0000-0000-000000000000".into(),
-        question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
-            incididunt ut labore et dolore magna aliqua. In aliquam sem fringilla ut. Enim tortor \
-            at auctor urna. Mattis vulputate enim nulla aliquet porttitor lacus. Interdum varius \
-            sit amet mattis vulputate enim nulla."
-            .into(),
-        created_at: "Jan 1, 1970".into(),
-    })
+pub async fn create_answer(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(answer): JsonAxum<Answer>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::create_answer(answer, answers_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-// TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
-//       The handler function should be called `read_answers`.
-//
-//       hint: this function should look very similar to the read_questions function above
-pub async fn read_answers() -> impl IntoResponse {
-    Json(vec![
-        AnswerDetail {
-            answer_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                tempor incididunt ut labore et dolore magna aliqua. Viverra aliquet eget sit amet \
-                tellus cras. Est sit amet facilisis magna etiam. Odio facilisis mauris sit amet. \
-                Diam sit amet nisl suscipit."
-                .into(),
-            created_at: "Jan 1, 1970".into(),
-        },
-        AnswerDetail {
-            answer_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            question_uuid: "00000000-0000-0000-0000-000000000000".into(),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
-                tempor incididunt ut labore et dolore magna aliqua. Imperdiet nulla malesuada \
-                pellentesque elit eget gravida cum sociis. Sed viverra tellus in hac habitasse \
-                platea dictumst vestibulum rhoncus. Sed id semper risus in hendrerit gravida. \
-                Vitae ultricies leo integer malesuada nunc vel."
-                .into(),
-            created_at: "Jan 1, 1970".into(),
-        },
-    ])
+pub async fn read_answers(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(question_uuid): JsonAxum<QuestionId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::read_answers(question_uuid, answers_dao.as_ref())
+        .await
+        .map(JsonAxum)
 }
 
-// TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
-//       The handler function should be called `delete_answer`.
-//
-//       hint: this function should look very similar to the delete_question function above
-pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) {
-    ()
+pub async fn delete_answer(
+    AxumState(AppState { answers_dao, .. }): AxumState<AppState>,
+    JsonAxum(answer_uuid): JsonAxum<AnswerId>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    handlers_inner::delete_answer(answer_uuid, answers_dao.as_ref()).await
 }
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,28 @@ extern crate log;
 
 extern crate pretty_env_logger;
 
-use axum::{
-    routing::{delete, get, post},
-    Router,
-};
-
-use dotenvy::dotenv;
-
 mod handlers;
 mod models;
 mod persistance;
 
+use::std::sync::Arc;
+use dotenvy::dotenv;
 use handlers::*;
 use sqlx::postgres::PgPoolOptions;
+use axum::{
+    routing::{delete, get, post},
+    Router,
+};
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -31,17 +40,26 @@ async fn main() {
                                                    .await
                                                    .expect("Failed to create Postgres connection pool!");
 
+    // Create DataAccessObject instances 
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool));
+
+    let app_state = AppState {questions_dao, answers_dao};
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/questions", get(read_questions))
         .route("/question", delete(delete_question))
         .route("/answer", post(create_answer))
         .route("/answers", get(read_answers))
-        .route("/answer", delete(delete_answer));
+        .route("/answer", delete(delete_answer))
+        .with_state(app_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
         .await
         .unwrap();
 
+    println!("Running on 127.0.0.1:8080");
+    
     axum::serve(listener, app).await.unwrap();
 }
